0a0274d2645cf7d59f5f832a636bebb86c62de97
ayesh-chamodye <ayeshchamodye@gmail.com>
2025-05-12 22:18:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
 import { defineConfig } from 'vite';
+import {fileURLToPath} from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 
 export default defineConfig({
   root: 'client',
