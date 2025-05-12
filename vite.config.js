7a9240887bb5059a32d6cf7c06941685a83fd172
ayesh-chamodye <ayeshchamodye@gmail.com>
2025-05-12 21:56:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: 'client',
   server: {
     hmr: {
       overlay: true,
     },
   },
   // ...existing code...
+ build: {
+ outDir: '../dist',
+ emptyOutDir: true,
+}, 
 }); 
